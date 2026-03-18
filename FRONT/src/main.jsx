c8567ca7919bc2e2cbcc0a9db10bc08ad28d3d7c
margar3tas <k.mikae.lieb@gmail.com>
2026-03-18 19:34:47 +0000

    bug fix

--- FRONT/src/main.jsx
+++ FRONT/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { io } from 'socket.io-client';
+// import { io } from 'socket.io-client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import AppProvider from '@/contexts/App/AppProvider.jsx';
@@ -11,17 +11,17 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import UserSessionProvider from '@/contexts/UserSession/UserSessionProvider.jsx';
 
 // SOCKET TEST FOR BACKEND
-const socket = io('http://localhost:3000');
+// const socket = io('http://localhost:3000');
 
-socket.on('connect', () => {
-  console.log('connected:', socket.id);
-});
+// socket.on('connect', () => {
+//   console.log('connected:', socket.id);
+// });
 
-socket.emit('ping');
-socket.on('pong', () => console.log('PONG'));
-//
-//
-//
+// socket.emit('ping');
+// socket.on('pong', () => console.log('PONG'));
+// //
+// //
+// //
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
